93eb80af0701f7455a116328e9c5caaea73bd632
Kyla Tingey <kylatingey97@gmail.com>
2017-03-27 10:51:06 -0700

    Integrated new and create actions for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,21 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(params.require(:product).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to products_path, notice: 'Your product was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
