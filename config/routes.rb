e08ba318b82674dc3a2be69b5a48978524453f49
Kyla Tingey <kylatingey97@gmail.com>
2017-03-27 13:41:10 -0700

    Customized product routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :blogs
-  resources :products
+  resources :products, except: [:show]
+  get 'product/:id', to: 'products#show', as: 'product_show'
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
