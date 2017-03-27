9aecb673474eb9b4da2ed77de5644ac086e69ff8
Kyla Tingey <kylatingey97@gmail.com>
2017-03-27 11:07:41 -0700

    Implemented edit and update functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,4 +19,20 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end 
+
+  def update
+    @product = Product.find(params[:id])
+    
+    respond_to do |format|
+      if @product.update(params.require(:product).permit(:title, :subtitle, :body))
+        format.html { redirect_to products_path, notice: 'Product was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
 end
