683f8bc45035d0c511d71f53f6ce017d9ccbd849
Kyla Tingey <kylatingey97@gmail.com>
2017-04-03 11:47:02 -0700

    Implemented image defaults for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
-  validates_presence_of :title, :body
+  validates_presence_of :title, :body, :main_image, :thumb_image
   belongs_to :category
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 end
