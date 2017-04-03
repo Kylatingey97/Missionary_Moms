ef2e115904bedcc568e9aa090ceb2b3dd647d0d5
Kyla Tingey <kylatingey97@gmail.com>
2017-04-03 11:46:10 -0700

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+3.times do |category|
+  Category.create!(
+    title: "Category #{category}"
+  )
+end
+
+puts "3 Categories created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}", 
@@ -13,7 +21,8 @@ puts "10 blog posts created"
     subtitle: "Description",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "http://placehold.it/600x400",
-    thumb_image: "http://placehold.it/350x200"
+    thumb_image: "http://placehold.it/350x200", 
+    category_id: Category.last.id
   )
 end
 
