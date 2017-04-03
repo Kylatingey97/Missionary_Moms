104a124464f6db879ce6c703b3e129144f6d87c5
Kyla Tingey <kylatingey97@gmail.com>
2017-04-03 11:04:09 -0700

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ puts "10 blog posts created"
 9.times do |product|
   Product.create!(
     title: "Product Name: #{product}",
-    subtitle: "It comes in different colors",
+    subtitle: "Description",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200"
@@ -18,8 +18,3 @@ puts "10 blog posts created"
 end
 
 puts "9 products created"
-
-
-
-
-
